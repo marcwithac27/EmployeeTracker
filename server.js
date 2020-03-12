3bfbd7b2cbeb7263e72e90650e97dfca8544aa65
Marc Maldonado <marc@telecomelite.com>
2020-03-11 22:35:34 -0400

    Starting the employee View again

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ function departmentsView(){
 }
 
 function employeeView(){
-    let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;";
+    let query = "SELECT * FROM employee";
     connection.query(query, (err,res) => {
     console.table(res) 
     next()
